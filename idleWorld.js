fb2de1b9851eb4900c89ab29fa8a34805470f643
hazardProgrammer <mlg.allen01@gmail.com>
2018-01-28 20:22:21 -0500

    Update idleWorld.js

--- idleWorld.js
+++ idleWorld.js
@@ -7,7 +7,6 @@ var canvas;
 var usernameInput;
 var usernameDiv;
 var ctx;
-var loginScreen;
 var username;
 var xp;
 var xpCalcLevel;
@@ -26,6 +25,7 @@ var monstersKilled;
 var monsterNames;
 var img;
 var overStartBtn;
+var upgradesBox;
 
 function init() {
 	canvas = $("#canvas")[0];
@@ -33,9 +33,10 @@ function init() {
 	usernameDiv = $("#username-div")[0];
 	ctx = canvas.getContext("2d");
 	if (localStorage.getItem("username") === null) {
-		loginScreen = true;
+		isMainScreen = false;
 	} else {
 		username = localStorage.getItem("username");
+		inMainScreen = true;
 	};
 
 	if (localStorage.getItem("xp") === null) {
@@ -51,7 +52,6 @@ function init() {
 	};
 	level = 0;
 	xpCalcLevel = 0;
-	inMainScreen = true;
 	monsterStats = {
 		killTime: 10,
 		level: 1,
@@ -96,6 +96,7 @@ function init() {
 		ctx.drawImage(img[1], 350, 250);
 	}
 	overStartBtn = false;
+	upgradesBox = false;
 
 	getMonsterHealth(monsterStats.level);
 	monsterStats.health = monsterStats.maxHealth;
@@ -104,7 +105,7 @@ function init() {
 }
 
 function renderMain() {
-	if (loginScreen) {
+	if (!inMainScreen) {
 		ctx.fillStyle = "#222222";
 		ctx.fillRect(0, 0, 800, 600);
 		ctx.fillStyle = "#603a0c";
@@ -136,103 +137,135 @@ function renderMain() {
 		ctx.font = "16px Arial";
 		ctx.fillStyle = "#000000";
 		ctx.textAlign = "left";
+		ctx.textBaseline = "middle";
 		ctx.fillText(username, 8, 20);
 		ctx.fillText("XP: " + xp, 8, 36);
 		ctx.fillText("Gold: " + gold, 8, 52);
 		ctx.fillText("Level: " + level, 8, 68);
+		ctx.fillStyle = "4d2001";
+		ctx.fillRect(405, 0, 80, 80);
+		ctx.fillStyle = "#7d4f20";
+		ctx.fillRect(410, 5, 70, 70);
+		ctx.fillStyle = "#ffffff";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "top";
+		ctx.font = "16px Arial";
+		ctx.fillText("Upgrades", 445, 10);
+		if (upgradesBox) {
+			ctx.fillStyle = "rgba(10, 10, 10, 0.8)";
+			ctx.fillRect(0, 0, 800, 600);
+			ctx.fillStyle = "#4d2001";
+			ctx.fillRect(100, 100, 600, 400);
+			ctx.fillStyle = "#7d4f20";
+			ctx.fillRect(105, 105, 590, 390);
+			ctx.fillStyle = "#ff6535";
+			ctx.fillRect(670, 70, 60, 60);
+			ctx.fillStyle = "#ffffff";
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+			ctx.font = "50px Arial";
+			ctx.fillText("X", 700, 100);
+			ctx.fillStyle = "#4d2001";
+			ctx.fillRect(250, 75, 300, 50);
+			ctx.fillStyle = "#7d4f20";
+			ctx.fillRect(255, 80, 290, 40);
+			ctx.font = "30px Arial";
+			ctx.fillStyle = "#ffffff";
+			ctx.fillText("Upgrades", 400, 100);
+		};
 	};
 }
 
 function killMonsters() {
-	if (!loginScreen) {
-		if (inMainScreen) {
-			if (!isMonsterDead) {
-				if (monsterStats.level % 2 === 1) {
-					ctx.drawImage(img[0], 350, 250);
-				} else {
-					ctx.drawImage(img[1], 350, 250);
-				};
-			};
-			ctx.fillStyle = "#413f45";
-			ctx.fillRect(200, 380, 400, 100);
-			ctx.fillStyle = "#ffffff";
-			ctx.textAlign = "left";
-			ctx.textBaseline = "middle";
-			ctx.fillText("LV. " + monsterStats.level, 210, 395);
-			ctx.textAlign = "right";
-			if (typeof monsterNames[monsterStats.level - 1] !== "undefined") {
-				ctx.fillText(monsterNames[monsterStats.level - 1], 590, 395);
+	if (inMainScreen) {
+		monsterStats.xpGain = Math.round((2 * monsterStats.level) ** (1.5 + (1 / 64) * Math.round(monsterStats.level / 25)));
+		monsterStats.goldGain = Math.round((monsterStats.level * 2) ** (1.3 + Math.floor(monsterStats.level / 25) * 0.015));
+		if (!isMonsterDead) {
+			if (monsterStats.level % 2 === 1) {
+				ctx.drawImage(img[0], 350, 250);
 			} else {
-				ctx.fillText(monsterNames[2 - (monsterStats.level % 2) - 1] + " " + Math.ceil(monsterStats.level / 2), 590, 395);
+				ctx.drawImage(img[1], 350, 250);
 			};
-			ctx.fillStyle = "#615f65";
-			ctx.fillRect(205, 410, 390, 30);
-			ctx.fillRect(205, 445, 390, 30);
-			var timeLeftRatio = (monsterStats.killTime - ((Date.now() - dateStart)/1000)).toFixed(2) / monsterStats.killTime;
-			if (timeLeftRatio <= 0 || monsterStats.health <= 0) {
-				isMonsterDead = true;
+		};
+		ctx.fillStyle = "#413f45";
+		ctx.fillRect(200, 380, 400, 100);
+		ctx.fillStyle = "#ffffff";
+		ctx.textAlign = "left";
+		ctx.textBaseline = "middle";
+		ctx.fillText("LV. " + monsterStats.level, 210, 395);
+		ctx.textAlign = "right";
+		if (typeof monsterNames[monsterStats.level - 1] !== "undefined") {
+			ctx.fillText(monsterNames[monsterStats.level - 1], 590, 395);
+		} else {
+			ctx.fillText(monsterNames[2 - (monsterStats.level % 2) - 1] + " " + Math.ceil(monsterStats.level / 2), 590, 395);
+		};
+		ctx.fillStyle = "#615f65";
+		ctx.fillRect(205, 410, 390, 30);
+		ctx.fillRect(205, 445, 390, 30);
+		var timeLeftRatio = (monsterStats.killTime - ((Date.now() - dateStart)/1000)).toFixed(2) / monsterStats.killTime;
+		if (monsterStats.health <= 0 || timeLeftRatio <= 0) {
+			isMonsterDead = true;
+			if (timeLeftRatio > 0) {
+				monstersKilled++;
 				gold += monsterStats.goldGain;
 				xp += monsterStats.xpGain;
-				monstersKilled++;
-				setTimeout(function() {
-					dateStart = Date.now();
-					isMonsterDead = false;
-				}, 1000);
-			};
-			ctx.fillStyle = "#a21ca3";
-			ctx.beginPath();
-			ctx.moveTo(560, 160);
-			ctx.lineTo(560 + 20 * Math.sqrt(3) / 2, 170);
-			ctx.lineTo(560, 180);
-			ctx.fill();
-			ctx.beginPath();
-			ctx.moveTo(240, 160);
-			ctx.lineTo(240 - 20 * Math.sqrt(3) / 2, 170);
-			ctx.lineTo(240, 180);
-			ctx.fill();
-			if (!isMonsterDead) {
-				var healthLeftRatio = monsterStats.health / monsterStats.maxHealth;
-				ctx.fillStyle = "#d62124";
-				ctx.fillRect(210, 415, 380 * healthLeftRatio, 20);
-				ctx.fillStyle = "#2222ca";
-				ctx.fillRect(210, 450, 380 * timeLeftRatio, 20);
-				ctx.fillStyle = "#ffffff";
-				ctx.font = "15px Arial";
-				ctx.textAlign = "center";
-				ctx.textBaseline = "middle";
-				ctx.fillText((monsterStats.killTime - ((Date.now() - dateStart)/1000)).toFixed(2) + " S", 400, 460);
-				ctx.fillText(monsterStats.health +  " / " + monsterStats.maxHealth, 400, 425);
-				i = frameChange % (charStats.autoStats.attackSpeed * 100);
-				if (i === 0 || i < 30) {
-					if (i === 0 && frameChange !== 0) {
-						monsterStats.health -= charStats.autoStats.autoDamage;
-					} else {
-						ctx.font = "32px Arial";
-						ctx.fillStyle = "rgba(255, 102, 0, " + (1 - (damageFloatX / 120));
-						ctx.fillText(charStats.autoStats.autoDamage, 400, 300 - damageFloatX);
-						damageFloatX += 3;
-					}
+			}
+			setTimeout(function() {
+				dateStart = Date.now();
+				isMonsterDead = false;
+			}, 1000);
+		};
+		ctx.fillStyle = "#a21ca3";
+		ctx.beginPath();
+		ctx.moveTo(560, 160);
+		ctx.lineTo(560 + 20 * Math.sqrt(3) / 2, 170);
+		ctx.lineTo(560, 180);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.moveTo(240, 160);
+		ctx.lineTo(240 - 20 * Math.sqrt(3) / 2, 170);
+		ctx.lineTo(240, 180);
+		ctx.fill();
+		if (!isMonsterDead) {
+			var healthLeftRatio = monsterStats.health / monsterStats.maxHealth;
+			ctx.fillStyle = "#d62124";
+			ctx.fillRect(210, 415, 380 * healthLeftRatio, 20);
+			ctx.fillStyle = "#2222ca";
+			ctx.fillRect(210, 450, 380 * timeLeftRatio, 20);
+			ctx.fillStyle = "#ffffff";
+			ctx.font = "15px Arial";
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+			ctx.fillText((monsterStats.killTime - ((Date.now() - dateStart)/1000)).toFixed(2) + " S", 400, 460);
+			ctx.fillText(monsterStats.health +  " / " + monsterStats.maxHealth, 400, 425);
+			i = frameChange % (charStats.autoStats.attackSpeed * 100);
+			if (i === 0 || i < 30) {
+				if (i === 0 && frameChange !== 0) {
+					monsterStats.health -= charStats.autoStats.autoDamage;
 				} else {
-					damageFloatX = 0;
-				};
+					ctx.font = "32px Arial";
+					ctx.fillStyle = "rgba(255, 102, 0, " + (1 - (damageFloatX / 120));
+					ctx.fillText(charStats.autoStats.autoDamage, 400, 300 - damageFloatX);
+					damageFloatX += 3;
+				}
 			} else {
-				monsterStats.health = monsterStats.maxHealth;
-				ctx.fillStyle = "#ff5500";
-				ctx.textAlign = "center";
-				ctx.textBaseline = "middle";
-				ctx.font = "48px Arial";
-				ctx.fillText("Monster Killed!", 400, 300);
-				frameChange = 0;
-				frameSinceClick = 0;
-				monsterStats.xpGain = Math.round((2 * monsterStats.level) ** (1.5 + (1 / 64) * Math.round(monsterStats.level / 25)));
-				monsterStats.goldGain = Math.round((monsterStats.level * 2) ** (1.3 + Math.floor(monsterStats.level / 25) * 0.015));
+				damageFloatX = 0;
 			};
+		} else {
+			monsterStats.health = monsterStats.maxHealth;
+			ctx.fillStyle = "#ff5500";
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+			ctx.font = "48px Arial";
+			ctx.fillText("Monster Killed!", 400, 300);
+			frameChange = 0;
+			frameSinceClick = 0;
 		};
 	};
 }
 
 function hideHTMLElements() {
-	if (!loginScreen) {
+	if (inMainScreen) {
 		usernameDiv.style.display = "none";
 	};
 }
@@ -249,27 +282,36 @@ function calcLevel() {
 function clickHandler(event) {
 	var clickX = event.clientX - canvas.getBoundingClientRect().left;
     var clickY = event.clientY - canvas.getBoundingClientRect().top;
-	if (loginScreen) {
+	if (!inMainScreen) {
 		if (clickX >= 320 && clickX <= 480 && clickY >= 335 && clickY <= 375) {
 			if (usernameInput.value.length <= 16) {
 				username = usernameInput.value;
 				localStorage.setItem("username", username);
 				console.log("Your username is " + username);
-				loginScreen = false;
+				inMainScreen = true;
 				dateStart = Date.now();
 			} else {
 				usernameInput.value = "";
 				alert("Sorry, your username is too long. Please make it 16 letters or shorter.")
 			};
 		};
-	} else if (inMainScreen) {
+	} else {
 		if (clickX >= 560 && clickX <= 560 + 40 * Math.sqrt(3) / 2 && clickY >= 160 && clickY <= 180) {
 			monsterStats.level++;
 		} else if (clickX <= 240 && clickX >= 240 - 40 * Math.sqrt(3) / 2 && clickY >= 160 && clickY <= 180 && monsterStats.level > 1) {
 			monsterStats.level--;
-		} else if (frameChange !== 0 && frameSinceClick / (charStats.clickStats.attackSpeed * 100) >= 1) {
-			frameSinceClick = 0;
-			monsterStats.health -= charStats.clickStats.clickDamage;
+		};
+		if (!upgradesBox) {
+			if (clickX >= 405 && clickX <= 485 && clickY >= 0 && clickY <= 80) {
+				upgradesBox = true;
+			} else if (frameChange !== 0 && frameSinceClick / (charStats.clickStats.attackSpeed * 100) >= 1) {
+				frameSinceClick = 0;
+				monsterStats.health -= charStats.clickStats.clickDamage;
+			};
+		} else {
+			if (clickX >= 670 && clickX <= 730 && clickY >= 70 && clickY <= 130) {
+				upgradesBox = false;
+			};
 		};
 	};
 }
@@ -277,7 +319,7 @@ function clickHandler(event) {
 function moveHandler(event) {
 	var mX = event.clientX - canvas.getBoundingClientRect().left;
     var mY = event.clientY - canvas.getBoundingClientRect().top;
-	if (loginScreen) {
+	if (!inMainScreen) {
 		if (mX >= 320 && mX <= 480 && mY >= 335 && mY <= 375) {
 			overStartBtn = true;
 		} else {
@@ -313,7 +355,7 @@ function resetGame() {
 	localStorage.removeItem("username");
 	localStorage.removeItem("xp");
 	localStorage.removeItem("gold");
-	localStorage.setItem("mLevel", monsterStats.level);
+	localStorage.removeItem("mLevel");
 }
 
 $("#canvas").click(clickHandler);
@@ -325,8 +367,8 @@ function run() {
 		ctx.strokeStyle = "#000000"
 		ctx.strokeRect(0, 0, 800, 600);
 		getMonsterHealth(monsterStats.level);
-		renderMain();
 		killMonsters();
+		renderMain();
 		hideHTMLElements();
 		calcLevel();
 		if (frame % 1000 === 0) {//saves every 10 seconds
